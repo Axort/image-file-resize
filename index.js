0a3006379f3f92344a20026d219b2b109f1aef24
Alex <alejandro.andrade@pixzelle.mx>
2021-11-02 20:20:35 -0600

    Changed resize logic

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-module.exports = function({ file, width, height, type }) {
+module.exports = function({ file, maxSize, type }) {
     return new Promise(function (resolve, reject) {
         let allow = ['jpg', 'gif', 'bmp', 'png', 'jpeg', 'svg'];
         try {
             if (file.name && file.name.split(".").reverse()[0] && allow.includes(file.name.split(".").reverse()[0].toLowerCase()) && file.size && file.type) {
                 let imageType = type ? type : 'jpeg';
-                const imgWidth = width ? width : 500;
-                const imgHeight = height ? height : 300;
+                // const imgWidth = width ? width : 500;
+                // const imgHeight = height ? height : 300;
+                const max_size = maxSize ? maxSize : 1024
                 const fileName = file.name;
                 const reader = new FileReader();
                 reader.readAsDataURL(file);
@@ -13,14 +14,30 @@ module.exports = function({ file, width, height, type }) {
                     const img = new Image();
                     img.src = event.target.result;
                     img.onload = () => {
-                        const elem = document.createElement('canvas');
-                        elem.width = imgWidth;
-                        elem.height = imgHeight;
+                        const elem = document.createElement('canvas'), width = img.width, height = img.height;
+
+                        if (width > height) {
+                            if (width > max_size) {
+                                height *= max_size / width;
+                                width = max_size;
+                            }
+                        } else {
+                            if (height > max_size) {
+                                width *= max_size / height;
+                                height = max_size;
+                            }
+                        }
+
+                        elem.width = width
+                        elem.height = height
+
+                        // elem.width = imgWidth;
+                        // elem.height = imgHeight;
                         const ctx = elem.getContext('2d');
-                        ctx.drawImage(img, 0, 0, imgWidth, imgHeight);
+                        ctx.drawImage(img, 0, 0, width, height);
                         ctx.canvas.toBlob((blob) => {
                             const file = new File([blob], fileName, {
-                                type: `image/${imageType.toLowerCase()}`,
+                                type: imageType.toLowerCase(),
                                 lastModified: Date.now()
                             });
                             resolve(file)
